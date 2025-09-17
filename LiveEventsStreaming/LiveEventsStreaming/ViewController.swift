e1b5d00cc385ffb13882334d10641dfad502dcac
Yves <yves.chen@aoitek.com>
2025-09-17 21:28:42 +0800

    init LiveEventsStreaming project

---
+++ LiveEventsStreaming/LiveEventsStreaming/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LiveEventsStreaming
+//
+//  Created by Yves on 2025/9/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
